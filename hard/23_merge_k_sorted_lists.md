1eb1895261da82822f64a007e9129d9404c38ce1
Yuan Jie Wong <56429440+YJwong325@users.noreply.github.com>
2025-06-11 11:55:02 -0400

    feat: [LC23] added markdown and python code files for first leetcode hard question "Merge k Sorted Lists"

---
+++ hard/23_merge_k_sorted_lists.md
@@ -0,0 +1,11 @@
+# LeetCode 23: Merge k Sorted Lists
+[Problem statement on LeetCode](https://leetcode.com/problems/merge-k-sorted-lists/)
+
+## Approach
+
+## Example 1:
+
+## Example 2:
+
+## Possible Considerations
+
---
+++ hard/23_merge_k_sorted_lists.py
@@ -0,0 +1,43 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def mergeKLists(self, lists):
+        # divide and conquer: divide the problem into 2 lists from many lists to make it more manageable
+        def merge(list1, list2):
+            empty = ListNode()
+            newList = empty
+
+            while list1 and list2:
+                if list1.val < list2.val:
+                    newList.next = list1
+                    list1 = list1.next
+                else:
+                    newList.next = list2
+                    list2 = list2.next
+                newList = newList.next
+            
+            if list1:
+                newList.next = list1
+            else:
+                newList.next = list2
+            
+            return empty.next
+
+        if not lists:
+            return None
+        
+        while len(lists) > 1:
+            newMerged = []
+
+            for i in range(0, len(lists), 2):
+                list1 = lists[i]
+                list2 = lists[i + 1] if i + 1 < len(lists) else None
+
+                newMerged.append(merge(list1, list2))
+            lists = newMerged
+        
+        return lists[0]
+
