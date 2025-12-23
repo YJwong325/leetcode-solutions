8d96eead4a7b80c096522707d3140ab4f2ea8495
Yuan Jie Wong <56429440+YJwong325@users.noreply.github.com>
2025-12-22 23:12:17 -0500

    [LC 22] add python code solution

---
+++ medium/22_generate_parentheses.py
@@ -0,0 +1,24 @@
+def generateParenthesis(n):
+    stack = []
+    res = []
+
+    def backtrack(openN, closedN):
+        if openN < n:
+            stack.append('(')
+            backtrack(openN + 1, closedN)
+            stack.pop()
+        if closedN < openN:
+            stack.append(')')
+            backtrack(openN, closedN + 1)
+            stack.pop()
+        if n == closedN == openN:
+            res.append("".join(stack))
+
+    backtrack(0, 0)
+    return res
+
+print("Different combinations generated for 3 pairs of parentheses:")
+print(generateParenthesis(3))
+
+print("\n\nDifferent combinations generated for 7 pairs of parentheses:")
+print(generateParenthesis(7))
